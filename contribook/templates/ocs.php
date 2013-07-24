4111277d1333deefd8faf0d972da1b9b8129b40c
Frank Karlitschek <frank@owncloud.org>
2013-07-24 14:31:09 +0200

    nice defaults

--- contribook/templates/ocs.php
+++ contribook/templates/ocs.php
@@ -6,7 +6,7 @@ foreach($_ as $item){
 
       if(isset($item['preview']) and !empty($item['preview'])) echo('<a href="'.$item['url'].'"><img src="'.$item['preview'].'" alt="'.$item['name'].'" title="'.$item['name'].'" /></a>');
 
-      echo('</td><td>');
+      echo('</td><td style="padding:10px;">');
 
       echo('<span class=""><a href="'.$item['url'].'">'.$item['name'].'</a></span><br />');
       if(!empty($item['description'])) echo(''.substr($item['description'],0,200).' ...');
